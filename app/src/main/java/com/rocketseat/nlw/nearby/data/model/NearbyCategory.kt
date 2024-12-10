73ea93e969bac98e9d9960de9eff306ae5701940
Nalisson Gomes <nalissongm.pro@gmail.com>
2024-12-10 18:29:17 -0300

    Components: Button, Category and list Category.

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/data/model/NearbyCategory.kt
@@ -0,0 +1,12 @@
+package com.rocketseat.nlw.nearby.data.model
+
+import androidx.annotation.DrawableRes
+
+data class NearbyCategory(
+    val id: String,
+    val name: String
+){
+    @get:DrawableRes
+    val icon: Int?
+        get() = NearbyCategoryFilterChipView.fromDescription(description = name)?.icon
+}
