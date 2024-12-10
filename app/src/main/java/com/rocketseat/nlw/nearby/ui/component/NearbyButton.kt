92287cc87de21026a8ba1240319206b9c91bfbd6
Nalisson Gomes <nalissongm.pro@gmail.com>
2024-12-10 00:42:56 -0300

    init commit

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/component/NearbyButton.kt
@@ -0,0 +1,28 @@
+package com.rocketseat.nlw.nearby.ui.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NearbyButton(
+    modifier: Modifier = Modifier,
+    text: String? = null,
+    @DrawableRes iconRes: Int? = null,
+    onClick: () -> Unit
+) {
+    Button(modifier= modifier, onClick = onClick) {
+        text?.let { Text(text = text ) }
+    }
+}
+
+
+@Preview
+@Composable
+private fun NearbyButtonPreview() {
+    NearbyButton(modifier = Modifier.fillMaxWidth(), text = "Confirmar", iconRes = null){}
+}
